241705ff187b3b447394d6e15613ac6433fc247c
KeatoonMask <ennes.finsi@gmail.com>
2020-04-13 19:41:37 +0200

    Include project structure

---
+++ BookList/BookList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookList
+//
+//  Created by Serafín Ennes Moscoso on 13/04/2020.
+//  Copyright © 2020 Serafín Ennes Moscoso. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
